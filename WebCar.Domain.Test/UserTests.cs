275b10fba34abc387e42b23ffda6ab9405aa31ae
LUIZ FELIPE MENDES PEREIRA RODRIGUES <2971392221003@fatecmatao.local>
2024-11-12 11:21:22 -0300

    add some logics

---
+++ WebCar.Domain.Test/UserTests.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace 
+{
+    public class WebCar.DomainTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var user = new User{
+                
+            }
+        }
+    }
+}
